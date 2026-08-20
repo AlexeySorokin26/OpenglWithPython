9682b5cb1b7393a4b5998220078245f5e4d20fb6
Alexey Sorokin <asorokin@gremse-it.com>
2026-08-20 23:03:34 +0200

    update

--- main.py
+++ main.py
@@ -11,44 +11,38 @@ screen_width = 1000
 screen_height = 800
 background_color = (0, 0, 0, 1)
 drawing_color = (1, 1, 1, 1)
-
+points = []
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
 pygame.display.set_caption('OpenGL in Python')
 
-
-def initialise():
-    glClearColor(background_color[0], background_color[1], background_color[2], background_color[3])
-    glColor(drawing_color)
-
-    # projection
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(60, (screen_width / screen_height), 0.1, 100.0)
-
-    # modelview
-    glMatrixMode(GL_MODELVIEW)
-    glTranslate(0, 0, -5)
-    glLoadIdentity()
-    glViewport(0, 0, screen.get_width(), screen.get_height())
-    glEnable(GL_DEPTH_TEST)
-    glTranslate(0, 0, -2)
-
-
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glRotatef(1, 10, 0, 1)
-    glPushMatrix()
-    wireCube()
-    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
+def plot_point():
+    glBegin(GL_POINTS)
+    for p in points:
+        glVertex2f(p[0], p[1])
+    glEnd()
+
+def init_ortho():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 1000, 800, 0)
 
 done = False
-initialise()
+init_ortho()
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == MOUSEBUTTONDOWN:
+            points.append(pygame.mouse.get_pos())
     display()
+    plot_point()
+
     pygame.display.flip()
     pygame.time.wait(100);
 pygame.quit()
